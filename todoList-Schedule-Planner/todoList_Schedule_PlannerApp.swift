2ea6427c226f59577944489cdc84e3db35385e39
NguyenDuyTrang <trangndps10349@gmail.com>
2023-11-30 09:40:28 +0700

    Initial Commit

---
+++ todoList-Schedule-Planner/todoList_Schedule_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoList_Schedule_PlannerApp.swift
+//  todoList-Schedule-Planner
+//
+//  Created by admin on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct todoList_Schedule_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
